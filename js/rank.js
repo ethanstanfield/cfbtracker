7b98e9441e0b23866644aa649d94c57cb2940dea
RTAlamo <jeremy.hicks1788@gmail.com>
2014-09-15 21:48:41 -0700

    Create rank.js
    This is just reference JS to proper rank each team, creating a new array each week with the updated rankings

---
+++ js/rank.js
@@ -0,0 +1,6 @@
+var teamRanks = [
+  ['fsu', 'bama', 'oregon', 'oklahoma', 'ohiostate', 'auburn', 'ucla', 'michiganstate', 'usce', 'baylor', 'stanford', 'uga', 'lsu', 'wiscy', 'uscw', 'clemson', 'notredame', 'olemiss', 'asu', 'kstate', 'texasam', 'nebraska', 'unc', 'mizzou'], 
+  ['fsu','bama','oregon','oklahoma','auburn','uga','michiganstate','ohiostate','texasam','baylor','ucla','lsu','stanford','uscw','olemiss','notredame','asu','wiscy','nebraska','kstate','usce','unc','clemson','mizzou'],
+  ['fsu', 'oregon', 'bama', 'oklahoma', 'auburn', 'uga', 'texasam', 'baylor', 'uscw', 'lsu', 'notredame', 'ucla', 'michiganstate', 'olemiss', 'stanford', 'asu', 'vatech', 'wiscy', 'kstate', 'mizzou', 'louisville', 'ohiostate', 'clemson', 'usce'],
+  ['fsu', 'bama', 'oregon', 'oklahoma', 'auburn', 'texasam', 'baylor', 'lsu', 'notredame', 'olemiss', 'michiganstate', 'ucla','uga','usce', 'asu', 'stanford', 'uscw','mizzou','wiscy','kstate','byu','clemson','ohiostate','nebraska','osu']
+  ];
